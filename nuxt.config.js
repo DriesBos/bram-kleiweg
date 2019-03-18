7c5f5b23ed652846f7637985d86994fca56f50bf
Dries Bos <info@driesbos.com>
2019-03-18 17:34:51 +0100

    comments

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,8 +55,8 @@ module.exports = {
       {
         accessToken:
           process.env.NODE_ENV === 'production' // Generate new token
-            ? 'LQu2PyUnQQnXdQrLxQ460Att'
-            : '2g0x0eQy87JuPexCzlBqYQtt',
+            ? 'IFkYnGTmkvPNorcAV0vEbwtt'
+            : 'a4koUFuhCdl89KPtRjGfewtt',
         cacheProvider: 'memory'
       }
     ]
