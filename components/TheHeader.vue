3499a6a53a6a7d8f459d28cb16811d5aab8799ce
Dries Bos <info@driesbos.com>
2019-03-19 21:13:56 +0100

    vertical header update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,12 +1,17 @@
 <template>
   <header>
-    <ul>
+    <ul class="header-Horizontal">
       <nuxt-link v-if="this.$route.name === 'blog'" class="header-Logo" tag="li" to="/">
         <p>bram kleiweg</p>
       </nuxt-link>
       <nuxt-link v-if="this.$route.name === 'blog'" class="header-Item" tag="li" to="/about">
         <p>about</p>
       </nuxt-link>
+      <li v-if="this.$route.name === 'blog-postId' || this.$route.name === 'about'">
+        <p>&nbsp;</p>
+      </li>
+    </ul>
+    <ul class="header-Vertical">
       <nuxt-link
         v-if="this.$route.name === 'blog-postId' || this.$route.name === 'about'"
         class="header-Item"
