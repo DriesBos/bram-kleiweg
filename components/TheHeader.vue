f9e0d4812f40752d0fb1640417c4a2368421a2ff
Dries Bos <info@driesbos.com>
2019-03-19 20:56:56 +0100

    header update

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,21 @@
 <template>
   <header>
-    <nuxt-link class="header-Logo" tag="div" to="/">
-      <a>bram kleiweg</a>
-      <span>1</span>
-    </nuxt-link>
     <ul>
-      <li>
-        <nuxt-link to="/about">about</nuxt-link>
-      </li>
+      <nuxt-link v-if="this.$route.name === 'blog'" class="header-Logo" tag="li" to="/">
+        <p>bram kleiweg</p>
+      </nuxt-link>
+      <nuxt-link v-if="this.$route.name === 'blog'" class="header-Item" tag="li" to="/about">
+        <p>about</p>
+      </nuxt-link>
+      <nuxt-link
+        v-if="this.$route.name === 'blog-postId' || this.$route.name === 'about'"
+        class="header-Item"
+        tag="li"
+        to="/blog"
+      >
+        <img src="@/assets/images/arrow.svg">
+        <p>back</p>
+      </nuxt-link>
     </ul>
   </header>
 </template>
