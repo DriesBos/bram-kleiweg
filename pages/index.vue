6ed39d50d8af8e1ce53101377a0a9202d952d562
Dries Bos <info@driesbos.com>
2019-03-19 22:15:43 +0100

    landing logo update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="view-Landing">
     <nuxt-link tag="div" to="/blog">
-      <h1>bram kleiweg</h1>
+      <h1 class="logo">bram kleiweg</h1>
     </nuxt-link>
   </section>
 </template>
