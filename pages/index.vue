23407bb3367e97ced15d75d1e146b2527772ac13
Dries Bos <info@driesbos.com>
2019-03-19 20:57:00 +0100

    landing

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,7 @@
 <template>
-  <section class="view-Horizontal view-Blog">
-    <ul class="post-List">
-      <PostPreview
-        v-for="post in posts"
-        :id="post.id"
-        :key="post.id"
-        :title="post.title"
-        :thumbnail="post.thumbnail"
-      />
-    </ul>
+  <section class="view-Landing">
+    <nuxt-link tag="div" to="/blog">
+      <h1>bram kleiweg</h1>
+    </nuxt-link>
   </section>
 </template>
-
-<script>
-import PostPreview from '~/components/PostPreview.vue'
-
-export default {
-  components: {
-    PostPreview: PostPreview
-  },
-  asyncData(context) {
-    return context.app.$storyapi
-      .get('cdn/stories', {
-        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft',
-        starts_with: 'blog/'
-      })
-      .then(res => {
-        console.log(res)
-        return {
-          posts: res.data.stories.map(bp => {
-            return {
-              id: bp.slug,
-              title: bp.content.title,
-              thumbnail: bp.content.thumbnail
-            }
-          })
-        }
-      })
-  }
-}
-</script>
