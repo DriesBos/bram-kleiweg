de3439a061ee934d6ecf0b4dbee2253391ccd17e
Dries Bos <info@driesbos.com>
2019-07-02 08:44:28 +0200

    added category to post-item

--- components/PostPreview.vue
+++ components/PostPreview.vue
@@ -4,7 +4,10 @@
       <h1>{{ title }}</h1>
     </nuxt-link>
     <nuxt-link :to="'/blog/' + id" tag="div" v-if="thumbnail" class="post-Thumbnail">
-      <img :src="thumbnail">
+      <img :src="thumbnail" />
+    </nuxt-link>
+    <nuxt-link :to="'/blog/' + id" tag="div" class="post-Category">
+      <h1>Category</h1>
     </nuxt-link>
   </li>
 </template>
