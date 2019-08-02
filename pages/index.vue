9fc2bec9653dba92d98bc1d4fa5ab1a207d04755
Dries Bos <info@driesbos.com>
2019-08-02 19:55:01 +0200

    landing hover effect

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <section class="view-Landing">
-    <nuxt-link tag="div" to="/blog">
-      <h1 class="logo">bram kleiweg</h1>
-    </nuxt-link>
+  <section class="view-Landing" :class="{ hover }">
+    <div @mouseover="mouseOver">
+      <nuxt-link tag="h1" to="/blog" class="logo">bram kleiweg</nuxt-link>
+    </div>
   </section>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      hover: false
+    }
+  },
+  methods: {
+    mouseOver: function() {
+      this.hover = true
+    }
+  }
+}
+</script>
