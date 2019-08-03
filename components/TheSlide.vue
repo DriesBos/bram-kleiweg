c3f2dada64ec62b51626f23400c0ba019a72d19c
Dries Bos <info@driesbos.com>
2019-08-03 22:33:05 +0200

    transition finetuning

--- components/TheSlide.vue
+++ components/TheSlide.vue
@@ -1,64 +1,59 @@
 <template>
   <section class="theSlide" :class="[{slider: slider}, {peek: peek}, {menu: menu}]">
     <!-- Landing text -->
-    <div v-if="slider" @click="toggleSlider" @mouseover="peek = true" @mouseleave="peek = false">
+    <div
+      v-if="slider"
+      @click="logoLandingClick"
+      @mouseover="peek = true"
+      @mouseleave="peek = false"
+    >
       <h1 class="logo">bram kleiweg</h1>
     </div>
 
-    <!-- Menu -->
-    <div
-      v-if="!slider"
-      @mouseover="menu = true"
-      @mouseleave="menu = false"
-      class="menu-Slider menu-Open"
-    >
+    <!-- Toggle -->
+    <div class="menu-Slider menu-Toggle">
       <ul>
-        <nuxt-link tag="li" to="/blog" v-bind:class="{active: !selectedCategory}">
-          <a>All Projects</a>
-        </nuxt-link>
-        <li
-          v-for="category in categories"
-          v-bind:class="{active: selectedCategory == category.id}"
-          v-bind:key="category.id"
-        >
-          <router-link tag="a" :to="'/category/' + category.id">{{ category.name }}</router-link>
-        </li>
-        <nuxt-link class="header-About header-Item" tag="li" to="/about">
-          <p>About</p>
-        </nuxt-link>
+        <transition name="menuFade">
+          <li v-if="!slider && !menu">
+            <img class="icon icon-Close" src="@/assets/images/hamburger.svg" />
+          </li>
+        </transition>
       </ul>
     </div>
 
-    <!-- Toggle -->
-    <div
-      v-if="!slider"
-      @mouseover="menu = true"
-      @mouseleave="menu = false"
-      class="menu-Slider menu-Toggle"
-    >
-      <ul>
-        <li v-if="menu">
-          <img class="icon icon-Close" src="@/assets/images/close.svg" />
-        </li>
-        <li v-if="!menu">
-          <img class="icon icon-Close" src="@/assets/images/hamburger.svg" />
-        </li>
-      </ul>
-    </div>
+    <div v-if="!menu && !slider" class="menu-Slider menu-Clickarea" @mouseover="menu = true"></div>
+
+    <!-- Menu -->
+    <transition name="menuFade">
+      <div v-if="menu" class="menu-Slider menu-Open" @mouseleave="menu = false">
+        <ul>
+          <nuxt-link tag="li" to="/blog" v-bind:class="{active: !selectedCategory}">
+            <a>All Projects</a>
+          </nuxt-link>
+          <li
+            v-for="category in categories"
+            v-bind:class="{active: selectedCategory == category.id}"
+            v-bind:key="category.id"
+          >
+            <router-link tag="a" :to="'/category/' + category.id">{{ category.name }}</router-link>
+          </li>
+          <nuxt-link class="header-About header-Item" tag="li" to="/about">
+            <p>About</p>
+          </nuxt-link>
+        </ul>
+      </div>
+    </transition>
 
     <!-- Logo -->
-    <div
-      v-if="!slider"
-      @click="toggleSlider"
-      @mouseover="menu = true"
-      class="menu-Slider menu-Logo"
-    >
-      <ul>
-        <li>
-          <p class="logo">bram kleiweg</p>
-        </li>
-      </ul>
-    </div>
+    <transition name="menuFade">
+      <div v-if="!slider" @click="logoClick" @mouseover="menu = true" class="menu-Slider menu-Logo">
+        <ul>
+          <li>
+            <p class="logo">bram kleiweg</p>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </section>
 </template>
 
@@ -75,14 +70,21 @@ export default {
   methods: {
     toggleSlider: function() {
       this.slider = !this.slider
-      this.menu = false
-      this.peek = false
     },
     toggleSliderPeek: function() {
       this.peek = !this.peek
     },
     toggleSliderMenu: function() {
       this.menu = !this.menu
+    },
+    logoLandingClick: function() {
+      this.slider = false
+      this.peek = false
+    },
+    logoClick: function() {
+      this.slider = true
+      this.peek = false
+      this.menu = false
     }
   },
   computed: mapState({
