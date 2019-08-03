c26b52bb31fed275e9c42335c66b878cf123687c
Dries Bos <info@driesbos.com>
2019-08-03 15:25:40 +0200

    TheSlide component

---
+++ components/TheSlide.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="view-Landing">
+    <div>
+      <h1 class="logo">bram kleiweg</h1>
+    </div>
+
+    <div class="menu-Toggle menu-Collapsed">
+      <ul>
+        <li>
+          <p class="logo">bram kleiweg</p>
+        </li>
+        <li>
+          <img class="icon icon-Close" src="@/assets/images/close.svg" />
+        </li>
+      </ul>
+    </div>
+
+    <div class="menu-Toggle menu-Open">
+      <ul>
+        <li>
+          <p class="logo">bram kleiweg</p>
+        </li>
+        <li>
+          <p class="logo">menu toggle</p>
+        </li>
+        <ul class="header-Filter">
+          <li>
+            <p>Projects</p>
+          </li>
+          <nuxt-link tag="li" to="/blog" v-bind:class="{active: !selectedCategory}">
+            <a>All Projects</a>
+          </nuxt-link>
+          <li
+            v-for="category in categories"
+            v-bind:class="{active: selectedCategory == category.id}"
+            v-bind:key="category.id"
+          >
+            <router-link tag="a" :to="'/category/' + category.id">{{ category.name }}</router-link>
+          </li>
+        </ul>
+        <nuxt-link class="header-About header-Item" tag="li" to="/about">
+          <p>About</p>
+        </nuxt-link>
+        <li>
+          <p>COUNTER</p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: mapState({
+    categories: state => state.categories.list,
+    selectedCategory(state) {
+      return this.$route.params.categoryId
+    }
+  })
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
+  <div class="view-General">
     <transition name="page" mode="out-in">
       <nuxt />
     </transition>
+    <TheSlide />
   </div>
 </template>
+
+<script>
+import TheSlide from '~/components/TheSlide.vue'
+
+export default {
+  components: {
+    TheSlide: TheSlide
+  }
+}
+</script>
